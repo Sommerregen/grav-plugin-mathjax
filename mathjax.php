cd7523eacb01bf34bd41f005e5d609c243a63fb9
Sommerregen <sommerregen@users.noreply.github.com>
2015-11-18 20:44:41 +0100

    :maple_leaf: Version Update - Release v1.4.0

--- mathjax.php
+++ mathjax.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * MathJax v1.3.2
+ * MathJax v1.4.0
  *
  * This plugin allows you to include math formulas in your web pages,
  * either using TeX and LaTeX notation, and/or as MathML.
@@ -9,7 +9,7 @@
  * http://benjamin-regler.de/license/
  *
  * @package     MathJax
- * @version     1.3.2
+ * @version     1.4.0
  * @link        <https://github.com/sommerregen/grav-plugin-mathjax>
  * @author      Benjamin Regler <sommerregen@benjamin-regler.de>
  * @copyright   2015, Benjamin Regler
